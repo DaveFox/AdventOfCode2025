e3b52fa34326a243b0015ccd62ac971c99519741
David Fox <david.fox@ge.com>
2025-12-03 15:39:07 +0000

    Day3 part 2

--- Day03/day03.go
+++ Day03/day03.go
@@ -16,10 +16,18 @@ func main() {
 	fileStr := fileToString()
 
 	//// Testcases
-	testInput := "987654321111111\n811111111111119\n234234234234278\n818181911112111"
-	fmt.Println(part1(testInput), "expect 357")
+	//testInput := "987654321111111\n811111111111119\n234234234234278\n818181911112111"
+	//fmt.Println(part1(testInput), "expect 357")
 
 	fmt.Println("Part 1 result:", part1(fileStr))
+
+	//// Testcases
+	//testInput := "987654321111111\n811111111111119\n234234234234278\n818181911112111"
+	//fmt.Println(maxValInAllowedSpace("287654321911111", 2))
+	//fmt.Println(part2("287654321911111\n811111111111119"))
+	//fmt.Println(part2(testInput)) // expect 987654321111,811111111119,434234234278, 888911112111 = 3121910778619
+
+	fmt.Println("Part 2 result:", part2(fileStr))
 	fmt.Println("Took: ", time.Since(start))
 }
 
@@ -57,10 +65,44 @@ func part1(testInput string) int {
 				max2 = jolt
 			}
 		}
-		//fmt.Println("max1", string(max1), "max2", string(max2))
+
 		combined, _ := strconv.Atoi(string(max1) + string(max2))
-		//fmt.Println(combined)
 		total += combined
 	}
 	return total
+}
+
+func part2(testInput string) int {
+	battaryLines := strings.Split(testInput, "\n")
+	total := 0
+	for i := 0; i < len(battaryLines); i++ {
+		remainingSpace := 12
+		currentBattery := battaryLines[i]
+		maxString := ""
+		for remainingSpace > 0 {
+			maxVal, newBattery := maxValInAllowedSpace(currentBattery, remainingSpace)
+			maxString += maxVal
+			currentBattery = newBattery
+			remainingSpace--
+		}
+		// fmt.Println(maxString)
+		asNumber, _ := strconv.Atoi(maxString)
+		total += asNumber
+	}
+	return total
+}
+
+func maxValInAllowedSpace(batteryLine string, remainingSpace int) (string, string) {
+	localMax := int32(0)
+	maxPos := 0
+	for pos, jolt := range batteryLine {
+		if pos > len(batteryLine)-remainingSpace {
+			break
+		}
+		if jolt > localMax {
+			localMax = jolt
+			maxPos = pos
+		}
+	}
+	return string(localMax), batteryLine[maxPos+1:]
 }
